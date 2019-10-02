3f3e46592652bb93de36794015c08ab109b60500
Lian <liulian1004@hotmail.com>
2019-10-01 23:07:33 -0700

    1

---
+++ 480-Binary-Tree-Paths/BinaryTreePaths.java
@@ -0,0 +1,72 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root of the binary tree
+     * @return: all root-to-leaf paths
+     */
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        // write your code here
+    List<String> paths= new ArrayList<>();
+    if(root == null) {
+        return paths;
+       }
+    List<String> leftPaths= binaryTreePaths(root.left); //最后一轮 ["2->4","2->5"]
+    List<String> rightPaths= binaryTreePaths(root.right);//最后一轮 ["3"]
+    for(String p: leftPaths){
+        paths.add(root.val + "->" + p);//循环,给leftpath的每个元素单独加“root.vol + -> " 变成['1 ->2 ->4','1->2->5']
+    }
+    for(String p: rightPaths){
+        paths.add(root.val + "->" + p);
+    }
+    if (paths.size() == 0) {
+        paths.add(""+root.val);// “”=>String ; String+任何类型=Stringval转string
+    }
+    return paths;
+    }
+
+    // public class Solution {
+    // /**
+    //  * @param root the root of the binary tree
+    //  * @return all root-to-leaf paths
+    //  */
+    // public List<String> binaryTreePaths(TreeNode root) {
+    //     List<String> result = new ArrayList<String>();
+    //     if (root == null) {
+    //         return result;
+    //     }
+    //     helper(root, String.valueOf(root.val), result);
+    //     return result;
+    // }
+
+    // private void helper(TreeNode root, String path, List<String> result) {
+    //     if (root == null) {
+    //         return;
+    //     }
+
+    //     if (root.left == null && root.right == null) {//叶子节点
+    //         result.add(path);
+    //         return;
+    //     }
+
+    //     if (root.left != null) {
+    //         helper(root.left, path + "->" + String.valueOf(root.left.val), result);
+             //root = 1 => helper = (2, ''1  -> 2'', result)
+    //     }
+
+    //     if (root.right != null) {
+    //         helper(root.right, path + "->" + String.valueOf(root.right.val), result);
+    //     }
+    // }
+}
---
+++ 482-Binary-Tree-Sum/README.md
@@ -0,0 +1,2 @@
+https://www.lintcode.com/problem/binary-tree-level-sum/description
+create a helper,define the depth
